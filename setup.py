3f282a72c54f44c7e2e0245bf135fc95a9f846bb
yijunxu <yijunx@umich.edu>
2022-04-11 23:38:09 +0800

    aee

--- setup.py
+++ setup.py
@@ -10,5 +10,7 @@ setup(
     entry_points='''
         [console_scripts]
         hello=hello:cli
-    ''',  # here hello=hello:cli -> means hello in the console is mapped to cli function in the hello.py
+    ''',  
+    # here hello=hello:cli -> means hello (the left hand side hello) in the console is mapped to cli function in the hello.py
+    # we can use rich to make the output colorful
 )
